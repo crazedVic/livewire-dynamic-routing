bc5b2a2eb15cb07a646c369cef605b5d35bb5f70
Frenchtoast0 <38544250+Frenchtoast0@users.noreply.github.com>
2021-09-16 17:12:10 -0600

    Revert "Revert "Revert "Broken commit -> need to revert"""
    This reverts commit b05569dc9bf2214ac1ff8a1c94f3f7427a20791b.

--- resources/views/livewire/notes/core.blade.php
+++ resources/views/livewire/notes/core.blade.php
@@ -2,15 +2,36 @@
     {{ $note->name }}<br>
     @switch($subview)
         @case("details")
-            <livewire:base.menu-component view="" render="livewire.notes.menu"/>
+            <livewire:notes.menu view="" />
             <livewire:notes.read :note="$note"/>
         @break
         @case("documents")
-            <livewire:base.menu-component :view="$subview" render="livewire.notes.menu"/>
+            <livewire:notes.menu :view="$subview" />
             <livewire:documents.index :owner="$note"/>
         @break
-        @case("documents-upload")
+         @case("documents-upload")
             <livewire:documents.upload :owner="$note"/>
+        @break
+        @case("documents-finance")
+            <livewire:notes.menu :view="$subview" />
+            <livewire:documents.index :owner="$note" dept="Finance"/>
+        @break
+        @case("documents-hr")
+            <livewire:notes.menu :view="$subview" />
+            <livewire:documents.index :owner="$note" dept="HR"/>
+        @break
+        @case("documents-legal")
+            <livewire:notes.menu :view="$subview" />
+            <livewire:documents.index :owner="$note" dept="Legal"/>
+        @break
+        @case("documents-finance-upload")
+            <livewire:documents.upload :owner="$note" dept="Finance"/>
+        @break
+        @case("documents-hr-upload")
+            <livewire:documents.upload :owner="$note" dept="HR"/>
+        @break
+        @case("documents-legal-upload")
+            <livewire:documents.upload :owner="$note" dept="Legal"/>
         @break
         @default
             {{abort(404)}}
