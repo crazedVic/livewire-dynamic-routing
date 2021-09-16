f52fd67287b095a35282f44ec857711522c59f57
Frenchtoast0 <38544250+Frenchtoast0@users.noreply.github.com>
2021-09-16 11:32:53 -0600

    Minor fix to prevent users from clicking the last segment of the url in breadcrumb

--- resources/views/livewire/breadcrumbs/firm.blade.php
+++ resources/views/livewire/breadcrumbs/firm.blade.php
@@ -1,7 +1,11 @@
 <div>
     <ul>
     @foreach ($routes as $route)
-        <li><a href="/{{$route['url']}}">{{$route['name']}}</a></li>
+        @if ($loop->index < (count($routes)-1))
+            <li><a href="/{{$route['url']}}">{{$route['name']}}</a></li>
+        @else
+            <li>{{$route['name']}}</li>
+        @endif
     @endforeach
     </ul>
 </div>
