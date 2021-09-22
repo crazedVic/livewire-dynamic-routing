a35c8cbb337d5bcd8a8bb14db154b9acfc8e3d9c
Frenchtoast0 <38544250+Frenchtoast0@users.noreply.github.com>
2021-09-22 17:26:42 -0600

    Removed some comments

--- resources/views/livewire/firms/core.blade.php
+++ resources/views/livewire/firms/core.blade.php
@@ -6,18 +6,7 @@
             <livewire:base.menu-component :view="$subview" render="livewire.firms.menu"/>
             <livewire:documents.index :owner="$firm"/>
         @break
-        {{-- @case("finance-documents")
-            <livewire:base.menu-component :view="$subview" render="livewire.firms.menu"/>
-            <livewire:documents.index :owner="$firm" dept="Finance"/>
-        @break
-        @case("hr-documents")
-            <livewire:base.menu-component :view="$subview" render="livewire.firms.menu"/>
-            <livewire:documents.index :owner="$firm" dept="HR"/>
-        @break
-        @case("legal-documents")
-            <livewire:base.menu-component :view="$subview" render="livewire.firms.menu"/>
-            <livewire:documents.index :owner="$firm" dept="Legal"/>
-        @break --}}
+        
         @case("details")
             <livewire:base.menu-component view="" render="livewire.firms.menu"/>
             <livewire:firms.read :firm="$firm"/>
@@ -25,16 +14,6 @@
         @case("documents-upload")
             <livewire:documents.upload :owner="$firm"/>
         @break
-
-        {{-- @case("finance-documents-upload")
-            <livewire:documents.upload :owner="$firm" dept="Finance"/>
-        @break
-        @case("hr-documents-upload")
-            <livewire:documents.upload :owner="$firm" dept="HR"/>
-        @break
-        @case("legal-documents-upload")
-            <livewire:documents.upload :owner="$firm" dept="Legal"/>
-        @break --}}
 
         @case('notes')
             <livewire:base.menu-component :view="$subview" render="livewire.firms.menu"/>
